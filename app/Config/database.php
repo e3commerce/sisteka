53ab90c1064297ef047724f077643b5baed926e1
Caio Lima <caiolima@brainme.com.br>
2019-01-11 16:32:56 -0200

    1

---
+++ app/Config/database.php
@@ -0,0 +1,17 @@
+<?php
+class DATABASE_CONFIG
+{
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '186.202.152.132',
+		'login' => 'sisteka_db',
+		'password' => 'c29072907',
+		'database' => 'sisteka_db',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+
+	
+}
